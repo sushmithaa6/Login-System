29502d347364002541791c0d2bb4241dd6a70ace
sushmithaa6 <115730872+sushmithaa6@users.noreply.github.com>
2023-06-05 23:45:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var objUser=
+[
+    {
+        username:"sushmitha",password:"1234"
+    },
+    {
+        username:"sush",password:"sush"
+    }
+]
+function getInfo()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    for(i=0;i<objUser.length;i++)
+    {
+        if(username==objUser[i].username && password==objUser[i].password)
+        {
+            console.log(username+" is logged in!")
+            alert("Login successful!");
+            return
+        }
+        else
+        {
+            alert("Invalid username or password. Please try again.");
+        }
+    }
+    console.log("Incorrect Username or Password");
+}
